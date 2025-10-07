4752abad594de7c8cb823048c0584a6e6599004e
Aman <rajaman9355@gmail.com>
2025-10-07 23:42:03 +0530

    Added Middleware

---
+++ Frontend/src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const clinifyAccessToken = request.cookies.get("clinifyAccessToken")?.value;
+  const clinifyRefreshToken = request.cookies.get("clinifyRefreshToken")?.value;
+  const isLoggedIn = !!clinifyAccessToken || !!clinifyRefreshToken;
+
+  const currentPath = request.nextUrl.pathname;
+
+  if (!isLoggedIn) {
+    if (currentPath === "/") {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (isLoggedIn) {
+    if (currentPath === "/") {
+      return NextResponse.redirect(new URL("/dashboard/staff", request.url));
+    }
+    return NextResponse.next();
+  }
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
