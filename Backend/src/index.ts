acaf48fa79f71403ca59591ea3a98c969cd5eefc
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2025-10-08 14:58:57 +0530

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -11,7 +11,7 @@ import dashboardRouter from "./routes/dashboard.route";
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT ? parseInt(process.env.PORT) : 4000;
 
 connectCloudinary();
 
