30063921984f3b331f51d71392078ed5836c5725
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2025-10-08 16:01:21 +0530

    Update next.config.ts

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["https://www.clinify.life"],
+  },
 };
 
 export default nextConfig;
